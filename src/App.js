7aa8f4a334a1451eeb0226b9391778c67fae7d6a
Michael Zhao <michaelzhao314@gmail.com>
2020-11-18 02:38:45 -0600

    Added name typing animation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,42 @@
+import React from 'react';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="name">Michael Zhao</div>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+        this.name = 'Michael Zhao';
+        this.nameCount = 0;
+        this.nameTimer = null;
+    }
+
+    componentDidMount() {
+        this.nameTimer = setInterval(() => {
+            // Stop the typing if end of the string
+            if (this.nameCount > this.name.length) {
+                clearInterval(this.nameTimer);
+                return;
+            }
+
+            // Get the substring of the current name count
+            var subName = this.name.substring(0, ++this.nameCount);
+            
+            // LOL replace U+0020 with U+2009 if the string ends in space
+            // That's because HTML doesn't display trailing spaces (U+0020)
+            if (subName.endsWith(' ')) subName = subName.replace(' ', ' ');
+
+            // Set the state to change the name typing display
+            this.setState({ name: subName });
+        }, 200);
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="name">{this.state.name}</div>
+            </div>
+        );
+    }
 }
 
 export default App;
