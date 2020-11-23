0cf0a21ced8b688b5fb549f4f48d6e21c5c37f3f
Michael Zhao <michaelzhao314@gmail.com>
2020-11-22 21:22:58 -0600

    Added info and links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.scss';
-
-const STARS = 100;
+import data from './data.json';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {};
-        this.name = 'Michael Zhao';
         this.nameCount = 0;
         this.nameTimer = null;
         this.canvas = React.createRef();
@@ -17,15 +15,15 @@ class App extends React.Component {
     resizeCanvas = () => {
         this.height = this.root.current.clientHeight;
         this.width = this.root.current.clientWidth;
-		this.canvas.current.height = this.height;
+        this.canvas.current.height = this.height;
         this.canvas.current.width = this.width;
 
         var ctx = this.canvas.current.getContext('2d');
         ctx.fillStyle = '#EEEEEE';
-        for (var i = 0; i < STARS; i++) {
+        for (var i = 0; i < data.stars; i++) {
             ctx.fillRect(randInt(1, this.width), randInt(1, this.height), 4, 4);
         }
-    }
+    };
 
     componentDidMount() {
         // Set background size
@@ -34,14 +32,14 @@ class App extends React.Component {
 
         this.nameTimer = setInterval(() => {
             // Stop the typing if end of the string
-            if (this.nameCount > this.name.length) {
+            if (this.nameCount > data.name.length) {
                 clearInterval(this.nameTimer);
                 return;
             }
 
             // Get the substring of the current name count
-            var subName = this.name.substring(0, ++this.nameCount);
-            
+            var subName = data.name.substring(0, ++this.nameCount);
+
             // LOL replace U+0020 with U+2009 if the string ends in space
             // That's because HTML doesn't display trailing spaces (U+0020)
             if (subName.endsWith(' ')) subName = subName.replace(' ', ' ');
@@ -57,7 +55,22 @@ class App extends React.Component {
                 <canvas className="background" ref={this.canvas}></canvas>
                 <div className="spacer"></div>
                 <div className="name">{this.state.name}</div>
-                <img className="earth" src="https://api.michaelzhao.xyz/images/michaelzhao/earth.png" alt=""></img>
+                <div className="info">
+                    <div className="info-item about">{data.about}</div>
+                    <div className="info-item earth-spacer">
+                        <img
+                            className="earth"
+                            src="https://api.michaelzhao.xyz/images/michaelzhao/earth.png"
+                            alt=""
+                        ></img>
+                    </div>
+                    <div className="info-item links">
+                        <a href={data.email}>Email</a><br />
+                        <a href={data.github}>Github</a><br />
+                        <a href={data.linkedin}>Linkedin</a><br />
+                        <a href={data.advocacy}>Advocacy Site</a><br />
+                    </div>
+                </div>
             </div>
         );
     }
